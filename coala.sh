0c239397c668339afe7ee5061623652612434716
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-02-04 18:43:08 +0100

    coala.sh: Add a script to manage coala
    This adds a small shell script that will help using coala.
    It can setup coala for this project by running
    `./coala.sh install`, run coala with `./coala.sh run`,
    or uninstall coala and its python environment with
    `./coala.sh clean`.
    
    Closes https://github.com/Kagamihime/VIlain/issues/3

---
+++ coala.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [[ "$1" = "install" ]]; then
+    python3 -m venv coala-venv
+    source coala-venv/bin/activate
+
+    git clone https://github.com/coala/coala
+    git clone https://github.com/coala/coala-bears
+    pip3 install coala/
+    pip3 install coala-bears/
+    rm -rf coala/
+    rm -rf coala-bears/
+
+    deactivate
+
+elif [[ "$1" = "run" ]]; then
+    source coala-venv/bin/activate
+
+    coala
+
+    deactivate
+
+elif [[ "$1" = "clean" ]]; then
+    if [[ -d "coala" ]]; then
+        rm -rf coala
+    fi
+
+    if [[ -d "coala-bears" ]]; then
+        rm -rf coala-bears
+    fi
+
+    if [[ -d "coala-venv" ]]; then
+        rm -rf coala-venv
+    fi
+
+else
+    echo "Help:"
+    echo "install    - Install coala in the project's root"
+    echo "run        - Run coala if it is already installed"
+    echo "clean      - Remove coala and the python environment from the project's root"
+fi
