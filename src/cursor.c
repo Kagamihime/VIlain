9bb5cc1a8abd907cfbb022828ab5d774a9673a09
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:47:14 +0100

    cursor.c: Add the cursor
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ src/cursor.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/cursor.h"
+
+int get_pos_x()
+{
+    return -1;
+}
+
+int get_pos_y()
+{
+    return -1;
+}
+
+void set_pos_x()
+{
+}
+
+void set_pos_y()
+{
+}
