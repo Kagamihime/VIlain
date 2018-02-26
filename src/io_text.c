ec8dde7067be412665e523d414f47e7cf598d553
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:47:54 +0100

    io_text.c: Add the input/output file
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ src/io_text.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/io_text.h"
+
+BUFFER *load_file(IO_TEXT * io_text, char *path)
+{
+    return NULL;
+}
+
+int save_buffer(IO_TEXT * io_text, char *path, BUFFER * buff)
+{
+    return -1;
+}
