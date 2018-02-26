4f002baed6ae2040ac8f55f420a848551cf69b99
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:52:28 +0100

    settings.h: Add the header for settings.c
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ include/settings.h
@@ -0,0 +1,26 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+typedef struct SETTINGS SETTINGS;
+
+void save(SETTINGS* sets, char* path);
+void load(SETTINGS* sets, char* path);
+int get_line_wrapping(SETTINGS* sets);
+char* get_save_shortcut(SETTINGS* sets);
+char* get_load_shortcut(SETTINGS* sets);
+char* get_settings_shortcut(SETTINGS* sets);
+char* get_copy_shortcut(SETTINGS* sets);
+char* get_cut_shortcut(SETTINGS* sets);
+char* get_paste_shortcut(SETTINGS* sets);
+char* get_toogle_selection_shortcut(SETTINGS* sets);
+
+void set_line_wrapping(int val, SETTINGS* sets);
+void set_save_shortcut(SETTINGS* sets,int val);
+void set_load_shortcut(SETTINGS* sets,int val);
+void set_settings_shortcut(SETTINGS* sets,char* val);
+void set_copy_shortcut(SETTINGS* sets,char* val);
+void set_cut_shortcut(SETTINGS* sets,char* val);
+void set_paste_shortcut(SETTINGS* sets,char* val);
+void set_toogle_selection_shortcut(SETTINGS* sets );
+
+#endif
