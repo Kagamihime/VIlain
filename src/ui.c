de694d91c0608694b5c21727da8994df638b53ee
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:49:32 +0100

    ui.c: Add the interface file
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ src/ui.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/buffer.h"
+#include "../include/ui.h"
+#include "../include/settings.h"
+#include "../include/cursor.h"
+
+void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col)
+{
+}
+
+void print_wrapped_text(BUFFER * buff, unsigned int first_line)
+{
+}
+
+void exec_user_action()
+{
+}
+
+void select_text(BUFFER * buff, CURSOR * curs)
+{
+}
+
+void print_status_bar(char *str)
+{
+}
+
+void loading_menu()
+{
+}
+
+void saving_menu()
+{
+}
+
+void settings_menu(SETTINGS * set)
+{
+}
