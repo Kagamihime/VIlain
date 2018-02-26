c57fe5f31f252c2626666ba11b58a2d4f3f7d4f5
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:50:14 +0100

    main.c: Add the main file
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/buffer.h"
+#include "../include/cursor.h"
+#include "../include/settings.h"
+#include "../include/io_text.h"
+#include "../include/ui.h"
+
+int main(void)
+{
+}
