e69715a85d557322e3064867600d072261a997a9
Gaëlle Marais <marais@informatique.univ-paris-diderot.fr>
2018-02-26 10:48:38 +0100

    settings.c: Add the settings file
    Closes https://github.com/Kagamihime/VIlain/issues/7

---
+++ src/settings.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/settings.h"
+
+void save(SETTINGS * sets, char *path)
+{
+}
+
+void load(SETTINGS * sets, char *path)
+{
+}
+
+int get_line_wrapping(SETTINGS * sets)
+{
+    return -1;
+}
+
+char *get_save_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_load_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_settings_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_copy_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_cut_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_paste_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+char *get_toogle_selection_shortcut(SETTINGS * sets)
+{
+    return NULL;
+}
+
+void set_line_wrapping(int val, SETTINGS * sets)
+{
+}
+
+void set_save_shortcut(SETTINGS * sets, int val)
+{
+}
+
+void set_load_shortcut(SETTINGS * sets, int val)
+{
+}
+
+void set_settings_shortcut(SETTINGS * sets, char *val)
+{
+}
+
+void set_copy_shortcut(SETTINGS * sets, char *val)
+{
+}
+
+void set_cut_shortcut(SETTINGS * sets, char *val)
+{
+}
+
+void set_paste_shortcut(SETTINGS * sets, char *val)
+{
+}
+
+void set_toogle_selection_shortcut(SETTINGS * sets)
+{
+}
