cfa020ceea24373933562d2c9e38761820c0cb11
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-03-02 18:20:21 +0100

    ui.h: Apply coala cleanups
    Closes https://github.com/Kagamihime/VIlain/issues/25

--- include/ui.h
+++ include/ui.h
@@ -4,14 +4,13 @@
 typedef struct CURSOR CURSOR;
 typedef struct SETTINGS SETTINGS;
 
-void print_text(BUFFER* buff, unsigned int first_line, unsigned int first_col);
-void print_wrapped_text(BUFFER* buff, unsigned int first_line);
+void print_text(BUFFER *buff, unsigned int first_line, unsigned int first_col);
+void print_wrapped_text(BUFFER *buff, unsigned int first_line);
 void exec_user_action();
-void select_text(BUFFER* buff, CURSOR* curs);
-void print_status_bar(char* str);
+void select_text(BUFFER *buff, CURSOR * curs);
+void print_status_bar(char *str);
 void loading_menu();
 void saving_menu();
-void settings_menu(SETTINGS* set);
-
+void settings_menu(SETTINGS *set);
 
 #endif
