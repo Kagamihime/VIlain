e1f6faf2da3af74794a0f54c226e3bad42afb3de
Gaëlle Marais <marais.gaelle@gmail.com>
2018-03-02 09:58:43 +0100

    settings.h: Edit the header for settings.c according UML's modification
    Closes https://github.com/Kagamihime/VIlain/issues/7

--- include/settings.h
+++ include/settings.h
@@ -14,13 +14,13 @@ char* get_cut_shortcut(SETTINGS* sets);
 char* get_paste_shortcut(SETTINGS* sets);
 char* get_toogle_selection_shortcut(SETTINGS* sets);
 
-void set_line_wrapping(int val, SETTINGS* sets);
-void set_save_shortcut(SETTINGS* sets,int val);
-void set_load_shortcut(SETTINGS* sets,int val);
+void set_line_wrapping(SETTINGS* sets,int val);
+void set_save_shortcut(SETTINGS* sets,char* val);
+void set_load_shortcut(SETTINGS* sets,char* val);
 void set_settings_shortcut(SETTINGS* sets,char* val);
 void set_copy_shortcut(SETTINGS* sets,char* val);
 void set_cut_shortcut(SETTINGS* sets,char* val);
 void set_paste_shortcut(SETTINGS* sets,char* val);
-void set_toogle_selection_shortcut(SETTINGS* sets );
+void set_toogle_selection_shortcut(SETTINGS* sets,char* val );
 
 #endif
