de5a72eeac76a7fcc60a326ee4e4d53a74a21619
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-03-02 18:17:25 +0100

    settings.h: Apply coala cleanups
    Closes https://github.com/Kagamihime/VIlain/issues/25

--- include/settings.h
+++ include/settings.h
@@ -3,24 +3,24 @@
 
 typedef struct SETTINGS SETTINGS;
 
-void save(SETTINGS* sets, char* path);
-void load(SETTINGS* sets, char* path);
-int get_line_wrapping(SETTINGS* sets);
-char* get_save_shortcut(SETTINGS* sets);
-char* get_load_shortcut(SETTINGS* sets);
-char* get_settings_shortcut(SETTINGS* sets);
-char* get_copy_shortcut(SETTINGS* sets);
-char* get_cut_shortcut(SETTINGS* sets);
-char* get_paste_shortcut(SETTINGS* sets);
-char* get_toogle_selection_shortcut(SETTINGS* sets);
+void save(SETTINGS *sets, char *path);
+void load(SETTINGS *sets, char *path);
+int get_line_wrapping(SETTINGS *sets);
+char *get_save_shortcut(SETTINGS *sets);
+char *get_load_shortcut(SETTINGS *sets);
+char *get_settings_shortcut(SETTINGS *sets);
+char *get_copy_shortcut(SETTINGS *sets);
+char *get_cut_shortcut(SETTINGS *sets);
+char *get_paste_shortcut(SETTINGS *sets);
+char *get_toogle_selection_shortcut(SETTINGS *sets);
 
-void set_line_wrapping(SETTINGS* sets,int val);
-void set_save_shortcut(SETTINGS* sets,char* val);
-void set_load_shortcut(SETTINGS* sets,char* val);
-void set_settings_shortcut(SETTINGS* sets,char* val);
-void set_copy_shortcut(SETTINGS* sets,char* val);
-void set_cut_shortcut(SETTINGS* sets,char* val);
-void set_paste_shortcut(SETTINGS* sets,char* val);
-void set_toogle_selection_shortcut(SETTINGS* sets,char* val );
+void set_line_wrapping(SETTINGS *sets, int val);
+void set_save_shortcut(SETTINGS *sets, char *val);
+void set_load_shortcut(SETTINGS *sets, char *val);
+void set_settings_shortcut(SETTINGS *sets, char *val);
+void set_copy_shortcut(SETTINGS *sets, char *val);
+void set_cut_shortcut(SETTINGS *sets, char *val);
+void set_paste_shortcut(SETTINGS *sets, char *val);
+void set_toogle_selection_shortcut(SETTINGS *sets, char *val);
 
 #endif
