b0b2da6bed53b4abd6ec033396858054282a98ca
David <dvotrgi@gmail.com>
2018-03-07 16:22:27 +0000

    src/cursor.c: Correct the initial state
    Values in the setters were lacking.
    
    Fixes https://github.com/Kagamihime/VIlain/issues/11

--- src/cursor.c
+++ src/cursor.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "../include/cursor.h"
 
+
+
 int get_pos_x()
 {
     return -1;
@@ -12,10 +14,12 @@ int get_pos_y()
     return -1;
 }
 
-void set_pos_x()
+void set_pos_x(int pos_x)
 {
+	
 }
 
-void set_pos_y()
+void set_pos_y(int pos_y)
 {
+
 }
