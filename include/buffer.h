0416d3c0d1f264ed1e945ef62361911148c98dbd
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-03-18 17:54:27 +0100

    buffer.h: Reorder functions in the API
    This places the `delete_*` functions before the `override_*` functions
    because the latter rely on the others so it is more consistent to
    put them in this order.
    
    Closes https://github.com/Kagamihime/VIlain/issues/35

--- include/buffer.h
+++ include/buffer.h
@@ -12,15 +12,15 @@ char *get_text(BUFFER * buff, unsigned int from_line, unsigned int from_col,
 int insert_char(BUFFER * buff, char c, unsigned int line, unsigned int col);
 int insert_line(BUFFER * buff, char *str, unsigned int line);
 int insert_text(BUFFER * buff, char *str, unsigned int line, unsigned int col);
+int delete_char(BUFFER * buff, unsigned int line, unsigned int col);
+int delete_line(BUFFER * buff, unsigned int line);
+int delete_text(BUFFER * buff, unsigned int from_line, unsigned int from_col,
+                unsigned int to_line, unsigned int to_col);
 int override_char(BUFFER * buff, char c, unsigned int line, unsigned int col);
 int override_line(BUFFER * buff, char *str, unsigned int line);
 int override_text(BUFFER * buff, char *str, unsigned int from_line,
                   unsigned int from_col, unsigned int to_line,
                   unsigned int to_col);
-int delete_char(BUFFER * buff, unsigned int line, unsigned int col);
-int delete_line(BUFFER * buff, unsigned int line);
-int delete_text(BUFFER * buff, unsigned int from_line, unsigned int from_col,
-                unsigned int to_line, unsigned int to_col);
 // Split the line at every occurencies of a line return
 int autosplit_line(BUFFER * buff, unsigned int line);
 int split_line_at(BUFFER * buff, unsigned int line, unsigned int col);
