5ffb61503f4cefb71331bbf7e8c92e20337e732c
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-03-23 00:12:34 +0100

    buffer.h: Make the integer arguments signed
    This makes all the arguments that are integers signed. This avoids
    many conditionals that are false because of the unsigned arithmetic.
    
    Closes https://github.com/Kagamihime/VIlain/issues/36

--- include/buffer.h
+++ include/buffer.h
@@ -5,29 +5,27 @@ typedef struct BUFFER BUFFER;
 
 BUFFER *new_buffer();
 void free_buffer(BUFFER * buff);
-char get_char(BUFFER * buff, unsigned int line, unsigned int col);
-char *get_line(BUFFER * buff, unsigned int line);
-char *get_text(BUFFER * buff, unsigned int from_line, unsigned int from_col,
-               unsigned int to_line, unsigned int to_col);
-int insert_char(BUFFER * buff, char c, unsigned int line, unsigned int col);
-int insert_line(BUFFER * buff, char *str, unsigned int line);
-int insert_text(BUFFER * buff, char *str, unsigned int line, unsigned int col);
-int delete_char(BUFFER * buff, unsigned int line, unsigned int col);
-int delete_line(BUFFER * buff, unsigned int line);
-int delete_text(BUFFER * buff, unsigned int from_line, unsigned int from_col,
-                unsigned int to_line, unsigned int to_col);
-int override_char(BUFFER * buff, char c, unsigned int line, unsigned int col);
-int override_line(BUFFER * buff, char *str, unsigned int line);
-int override_text(BUFFER * buff, char *str, unsigned int from_line,
-                  unsigned int from_col, unsigned int to_line,
-                  unsigned int to_col);
+char get_char(BUFFER * buff, int line, int col);
+char *get_line(BUFFER * buff, int line);
+char *get_text(BUFFER * buff, int from_line, int from_col,
+               int to_line, int to_col);
+int insert_char(BUFFER * buff, char c, int line, int col);
+int insert_line(BUFFER * buff, char *str, int line);
+int insert_text(BUFFER * buff, char *str, int line, int col);
+int delete_char(BUFFER * buff, int line, int col);
+int delete_line(BUFFER * buff, int line);
+int delete_text(BUFFER * buff, int from_line, int from_col,
+                int to_line, int to_col);
+int override_char(BUFFER * buff, char c, int line, int col);
+int override_line(BUFFER * buff, char *str, int line);
+int override_text(BUFFER * buff, char *str, int from_line,
+                  int from_col, int to_line, int to_col);
 // Split the line at every occurencies of a line return
-int autosplit_line(BUFFER * buff, unsigned int line);
-int split_line_at(BUFFER * buff, unsigned int line, unsigned int col);
-int join_lines(BUFFER * buff, unsigned int from_line, unsigned int to_line,
-               int with_spaces);
-unsigned int get_line_count(BUFFER * buff);
-unsigned int get_line_length(BUFFER * buff, unsigned int line);
-unsigned int get_buffer_length(BUFFER * buff);
+int autosplit_line(BUFFER * buff, int line);
+int split_line_at(BUFFER * buff, int line, int col);
+int join_lines(BUFFER * buff, int from_line, int to_line, int with_spaces);
+int get_line_count(BUFFER * buff);
+int get_line_length(BUFFER * buff, int line);
+int get_buffer_length(BUFFER * buff);
 
 #endif
