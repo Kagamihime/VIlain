6c8b43e5667133c1cb9e152faa3286f5737213ee
David <dvotrgi@gmail.com>
2018-03-30 14:32:24 +0100

    settings.h: Adjust the API
    Closes https://github.com/Kagamihime/VIlain/issues/10

--- include/settings.h
+++ include/settings.h
@@ -6,6 +6,7 @@ typedef struct SETTINGS SETTINGS;
 void save(SETTINGS * sets, char *path);
 void load(SETTINGS * sets, char *path);
 int get_line_wrapping(SETTINGS * sets);
+void set_line_wrapping(SETTINGS * sets, int val);
 char *get_save_shortcut(SETTINGS * sets);
 char *get_load_shortcut(SETTINGS * sets);
 char *get_settings_shortcut(SETTINGS * sets);
@@ -14,7 +15,6 @@ char *get_cut_shortcut(SETTINGS * sets);
 char *get_paste_shortcut(SETTINGS * sets);
 char *get_toogle_selection_shortcut(SETTINGS * sets);
 
-void set_line_wrapping(SETTINGS * sets, int val);
 void set_save_shortcut(SETTINGS * sets, char *val);
 void set_load_shortcut(SETTINGS * sets, char *val);
 void set_settings_shortcut(SETTINGS * sets, char *val);
