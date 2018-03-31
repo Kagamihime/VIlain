3fd1794a2f072e5bf1d82f5c806d6d6d29f84199
Gaëlle Marais <marais.gaelle@gmail.com>
2018-03-31 10:18:42 +0200

    ui.h: Adjust API
    This adds some arguments that were missing
     and also a new function print_menu
    
    Closes https://github.com/Kagamihime/VIlain/issues/8

--- include/ui.h
+++ include/ui.h
@@ -1,14 +1,17 @@
 #ifndef UI_H
 #define UI_H
 
+#include <ncurses.h>
+
 typedef struct CURSOR CURSOR;
 typedef struct SETTINGS SETTINGS;
 
 void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col);
 void print_wrapped_text(BUFFER * buff, unsigned int first_line);
-void exec_user_action();
+void exec_user_action(BUFFER * buff);
 void select_text(BUFFER * buff, CURSOR * curs);
 void print_status_bar(char *str);
+void print_menu(WINDOW * menu_win, int highlight);
 void loading_menu();
 void saving_menu();
 void settings_menu(SETTINGS * set);
