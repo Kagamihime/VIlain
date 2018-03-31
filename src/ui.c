5e5134c6b1a2e30d9eacc427cd0250790813a61d
Gaëlle Marais <marais.gaelle@gmail.com>
2018-03-31 10:33:41 +0200

    ui.c: Implement the cursor movements
    This is in the function exec_user_action
    
    Closes https://github.com/Kagamihime/VIlain/issues/8

--- src/ui.c
+++ src/ui.c
@@ -45,13 +45,13 @@ void print_wrapped_text(BUFFER * buff, unsigned int first_line)
 void exec_user_action(BUFFER * buff)
 {
     //Creation of the window and the cursor
-    text_win = newwin(TEXT_HEIGHT,TEXT_WIDTH,0,0);
-    curs=new_curs();
+    text_win = newwin(TEXT_HEIGHT, TEXT_WIDTH, 0, 0);
+    curs = new_curs();
 
     //Print the current buffer and  place the cursor at the end
-    print_text(buff,0,0);
-    set_pos_y(curs,get_line_count(buff)-1);
-    set_pos_x(curs,get_line_length(buff,get_line_count(buff)-1));
+    print_text(buff, 0, 0);
+    set_pos_y(curs, get_line_count(buff) - 1);
+    set_pos_x(curs, get_line_length(buff, get_line_count(buff) - 1));
     wmove(text_win, get_pos_y(curs), get_pos_x(curs));
 
     //Set up the parameters to listen to keyboard events and enter the loop
@@ -60,6 +60,37 @@ void exec_user_action(BUFFER * buff)
     char *tmp;
     noecho();
     int exit = 0;
+
+    // Wait for the user to press a key
+    while (!exit) {
+        ch = wgetch(text_win);
+        switch (ch) {
+        case KEY_UP:           //UP ARROW: move cursor up
+            if (get_pos_y(curs) > 0)
+                set_pos_y(curs, get_pos_y(curs) - 1);
+            break;
+        case KEY_DOWN:         //DOWN ARROW: move cursor down
+            if (get_pos_y(curs) < TEXT_HEIGHT)
+                set_pos_y(curs, get_pos_y(curs) + 1);
+            break;
+        case KEY_RIGHT:        //RIGHT ARROW: move cursor right
+            if (get_pos_x(curs) < TEXT_WIDTH)
+                set_pos_x(curs, get_pos_x(curs) + 1);
+            break;
+        case KEY_LEFT:         //LEFT ARROW: move cursor left
+            if (get_pos_x(curs) > 0)
+                set_pos_x(curs, get_pos_x(curs) - 1);
+            break;
+        default:
+            insert_char(buff, ch, get_pos_y(curs), get_pos_x(curs));
+            set_pos_x(curs, get_pos_x(curs) + 1);
+            break;
+        }
+        print_text(buff, 0, 0);
+        wmove(text_win, get_pos_y(curs), get_pos_x(curs));
+        if (exit == 1)
+            break;
+    }
 }
 
 void select_text(BUFFER * buff, CURSOR * curs)
