5c94d4382260f3fbb3d1076ee11e41b81a7c112c
Gaëlle Marais <marais.gaelle@gmail.com>
2018-03-31 10:50:15 +0200

    main.c: Add test to launch the ui
    Closes https://github.com/Kagamihime/VIlain/issues/8

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ncurses.h>
 #include "../include/buffer.h"
 #include "../include/cursor.h"
 #include "../include/settings.h"
@@ -8,4 +9,20 @@
 
 int main(void)
 {
+    struct BUFFER *buff;
+    buff = new_buffer();
+    char *str1 = "Hello world";
+    char *str2 = "Hello jack";
+    char *str3 = "Hello billy";
+
+    insert_line(buff, str1, 0);
+    insert_line(buff, str2, 1);
+    insert_line(buff, str3, 2);
+
+    initscr();
+    exec_user_action(buff);
+    getch();
+    endwin();
+
+    return 0;
 }
