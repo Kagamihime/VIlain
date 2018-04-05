14d18c91726451aaa21e2755b1f1034916680b63
Gaëlle Marais <marais.gaelle@gmail.com>
2018-04-05 14:55:47 +0200

    ui.h: Add the function in header
    Closes https://github.com/Kagamihime/VIlain/issues/65

--- include/ui.h
+++ include/ui.h
@@ -8,6 +8,7 @@ typedef struct SETTINGS SETTINGS;
 
 void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col);
 void print_wrapped_text(BUFFER * buff, unsigned int first_line);
+void move_cursor(BUFFER *buff,CURSOR *curs, int ch);
 void exec_user_action(BUFFER * buff);
 void select_text(BUFFER * buff, CURSOR * curs);
 void print_status_bar(char *str);
