fb905796bc2e5cfe2c6642030ef0bbcf9d972630
Gaëlle Marais <marais.gaelle@gmail.com>
2018-04-05 15:02:38 +0200

    ui/c: Improve move_cursor
    This prevents the user from moving to
    positions where the buffer is empty
    
    Closes https://github.com/Kagamihime/VIlain/issues/65

--- src/ui.c
+++ src/ui.c
@@ -109,10 +109,14 @@ void move_cursor(BUFFER * buff, CURSOR * curs, int ch)
     case KEY_UP:               //UP ARROW: move cursor up
         if (get_pos_y(curs) > 0)
             set_pos_y(curs, get_pos_y(curs) - 1);
+        if (get_pos_x(curs) > get_line_length(buff, get_pos_y(curs)) - 1)
+            set_pos_x(curs, get_line_length(buff, get_pos_y(curs)) - 1);
         break;
     case KEY_DOWN:             //DOWN ARROW: move cursor down
         if (get_pos_y(curs) < get_line_count(buff) - 1)
             set_pos_y(curs, get_pos_y(curs) + 1);
+        if (get_pos_x(curs) > get_line_length(buff, get_pos_y(curs)) - 1)
+            set_pos_x(curs, get_line_length(buff, get_pos_y(curs)) - 1);
         break;
     case KEY_RIGHT:            //RIGHT ARROW: move cursor right
         if (get_pos_x(curs) < get_line_length(buff, get_pos_y(curs)) - 1)
