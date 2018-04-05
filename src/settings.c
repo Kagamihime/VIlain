47118757f6cc8cf51ca61def40024a583087e651
David <dvotrgi@gmail.com>
2018-04-05 14:42:57 +0100

    src/settings.c: Adjust the load function
    Closes https://github.com/Kagamihime/VIlain/issues/64

--- src/settings.c
+++ src/settings.c
@@ -33,14 +33,52 @@ void save(SETTINGS * sets, char *path)
 
 void load(SETTINGS * sets, char *path)
 {
-    // TODO: implement this function
-    // char *line = "";
-    // FILE *file;
-    // file = fopen(path, "r");
-    // while (fgets(line, LINE_MAX_LENGTH, file) != NULL) {
-    //
-    // }
-    // fclose(file);
+    int i = 0;
+    int val;
+    char *val_string = malloc(sizeof(char *) * LINE_MAX_LENGTH);
+    char *param = malloc(sizeof(char *) * LINE_MAX_LENGTH);
+    char *line = malloc(sizeof(char *) * LINE_MAX_LENGTH);
+    FILE *file;
+    file = fopen(path, "r");
+    while (fgets(line, LINE_MAX_LENGTH, file) != NULL) {
+        if (i == 0) {
+            sscanf(line, "%s %d", param, &val);
+            set_line_wrapping(sets, val);
+        }
+        if (i == 1) {
+            sscanf(line, "%s %s", param, val_string);
+            set_save_shortcut(sets, val_string);
+        }
+        if (i == 2) {
+            sscanf(line, "%s %s", param, val_string);
+            set_load_shortcut(sets, val_string);
+        }
+        if (i == 3) {
+            sscanf(line, "%s %s", param, val_string);
+            set_settings_shortcut(sets, val_string);
+        }
+        if (i == 4) {
+            sscanf(line, "%s %s", param, val_string);
+            set_copy_shortcut(sets, val_string);
+        }
+        if (i == 5) {
+            sscanf(line, "%s %s", param, val_string);
+            set_cut_shortcut(sets, val_string);
+        }
+        if (i == 6) {
+            sscanf(line, "%s %s", param, val_string);
+            set_paste_shortcut(sets, val_string);
+        }
+        if (i == 7) {
+            sscanf(line, "%s %s", param, val_string);
+            set_toogle_selection_shortcut(sets, val_string);
+        }
+        i++;
+    }
+    free(val_string);
+    free(param);
+    free(line);
+    fclose(file);
 }
 
 int get_line_wrapping(SETTINGS * sets)
