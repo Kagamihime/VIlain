7cf5129c7fdc05f686defc85bea68be748b39121
David <dvotrgi@gmail.com>
2018-04-10 18:11:05 +0100

    io_text.c: Apply coala.sh run
    Closes https://github.com/Kagamihime/VIlain/issues/13

--- src/io_text.c
+++ src/io_text.c
@@ -5,22 +5,20 @@
 #define FILE_MAX_LENGTH 8192
 #define LINE_MAX_LENGTH 512
 
-struct IO_TEXT
-{
-	char ** buffer_text;
+struct IO_TEXT {
+    char **buffer_text;
 };
 
 BUFFER *load_file(IO_TEXT * io_text, char *path)
 {
-	int i = 0;
+    int i = 0;
     BUFFER *res = new_buffer();
     char *line = malloc(sizeof(char *) * LINE_MAX_LENGTH);
     FILE *file;
     file = fopen(path, "r");
-    while (fgets(line, FILE_MAX_LENGTH, file) != NULL)
-    {
-    	insert_line(res, line, i);
-    	i++;
+    while (fgets(line, FILE_MAX_LENGTH, file) != NULL) {
+        insert_line(res, line, i);
+        i++;
     }
     free(line);
     fclose(file);
