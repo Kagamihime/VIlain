5fe6b97a58237a28f380e201e048c501549a1e70
David <dvotrgi@gmail.com>
2018-04-13 14:34:24 +0100

    io_text.h: Adjust the API
    This deletes structures definition that were useless for the project.
    
    Closes https://github.com/Kagamihime/VIlain/issues/13

--- include/io_text.h
+++ include/io_text.h
@@ -1,10 +1,7 @@
 #ifndef IO_TEXT_H
 #define IO_TEXT_H
 
-typedef struct IO_TEXT IO_TEXT;
-typedef struct BUFFER BUFFER;
-
-BUFFER *load_file(IO_TEXT * io_text, char *path);
-int save_buffer(IO_TEXT * io_text, char *path, BUFFER * buff);
+BUFFER *load_file(char *path);
+int save_buffer(char *path, BUFFER * buff);
 
 #endif
