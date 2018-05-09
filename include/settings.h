0dd642df1d5dd28ada66d35d1b07f6306b013435
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-09 14:08:58 +0200

    settings.h: Adjust the API
    Closes https://github.com/Kagamihime/VIlain/issues/81

--- include/settings.h
+++ include/settings.h
@@ -5,22 +5,23 @@ typedef struct SETTINGS SETTINGS;
 
 void save(SETTINGS * sets, char *path);
 int load(SETTINGS * sets, char *path);
-int get_line_wrapping(SETTINGS * sets);
-void set_line_wrapping(SETTINGS * sets, int val);
-char *get_save_shortcut(SETTINGS * sets);
-char *get_load_shortcut(SETTINGS * sets);
-char *get_settings_shortcut(SETTINGS * sets);
-char *get_copy_shortcut(SETTINGS * sets);
-char *get_cut_shortcut(SETTINGS * sets);
-char *get_paste_shortcut(SETTINGS * sets);
-char *get_toogle_selection_shortcut(SETTINGS * sets);
 
-void set_save_shortcut(SETTINGS * sets, char *val);
-void set_load_shortcut(SETTINGS * sets, char *val);
-void set_settings_shortcut(SETTINGS * sets, char *val);
-void set_copy_shortcut(SETTINGS * sets, char *val);
-void set_cut_shortcut(SETTINGS * sets, char *val);
-void set_paste_shortcut(SETTINGS * sets, char *val);
-void set_toogle_selection_shortcut(SETTINGS * sets, char *val);
+int get_line_wrapping(SETTINGS * sets);
+int get_save_shortcut(SETTINGS * sets);
+int get_load_shortcut(SETTINGS * sets);
+int get_settings_shortcut(SETTINGS * sets);
+int get_copy_shortcut(SETTINGS * sets);
+int get_cut_shortcut(SETTINGS * sets);
+int get_paste_shortcut(SETTINGS * sets);
+int get_toogle_selection_shortcut(SETTINGS * sets);
+
+void set_line_wrapping(SETTINGS * sets, int val);
+void set_save_shortcut(SETTINGS * sets, int val);
+void set_load_shortcut(SETTINGS * sets, int val);
+void set_settings_shortcut(SETTINGS * sets, int val);
+void set_copy_shortcut(SETTINGS * sets, int val);
+void set_cut_shortcut(SETTINGS * sets, int val);
+void set_paste_shortcut(SETTINGS * sets, int val);
+void set_toogle_selection_shortcut(SETTINGS * sets, int val);
 
 #endif
