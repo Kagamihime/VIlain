b4c9456c9e2cc8327d1b83d74aad97eac99a1598
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-10 12:09:24 +0200

    ui.h: Adjust API with new parameters
    Closes https://github.com/Kagamihime/VIlain/issues/96

--- include/ui.h
+++ include/ui.h
@@ -6,15 +6,19 @@
 typedef struct CURSOR CURSOR;
 typedef struct SETTINGS SETTINGS;
 
+void print_menu(WINDOW * menu_win, int highlight, char **choices,
+                int number_choices);
+int move_in_menu(WINDOW * menu_win, int highlight, char **menu_choices,
+                 int number_choices);
+void shortcuts_menu(SETTINGS * sets);
+void settings_menu(SETTINGS * set);
 void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col);
 void print_wrapped_text(BUFFER * buff, unsigned int first_line);
 void move_cursor(BUFFER * buff, CURSOR * curs, int ch);
 void exec_user_action(BUFFER * buff);
 void select_text(BUFFER * buff, CURSOR * curs);
 void print_status_bar(BUFFER * buff, char *str);
-void print_menu(WINDOW * menu_win, int highlight);
 void loading_menu();
 void saving_menu();
-void settings_menu(SETTINGS * set);
 
 #endif
