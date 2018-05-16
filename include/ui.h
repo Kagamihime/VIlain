187098f2c68c1dd4c770ab6a51b3ca7b1537beb5
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-16 11:54:54 +0200

    ui.h: Adjust API
    Closes https://github.com/Kagamihime/VIlain/issues/109

--- include/ui.h
+++ include/ui.h
@@ -11,14 +11,14 @@ void print_menu(WINDOW * menu_win, int highlight, char **choices,
 int move_in_menu(WINDOW * menu_win, int highlight, char **menu_choices,
                  int number_choices);
 void shortcuts_menu(SETTINGS * sets);
-void settings_menu(SETTINGS * set);
+void settings_menu(SETTINGS * set, BUFFER *buff);
 void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col);
 void print_wrapped_text(BUFFER * buff, unsigned int first_line);
 void move_cursor(BUFFER * buff, CURSOR * curs, int ch);
 void exec_user_action(BUFFER * buff);
 void select_text(BUFFER * buff, CURSOR * curs, SETTINGS * sets);
 void print_status_bar(BUFFER * buff, char *str);
-void loading_menu();
-void saving_menu();
+void loading_file();
+void saving_file(BUFFER *buff);
 
 #endif
