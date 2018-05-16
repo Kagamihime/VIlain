89ecac18d2142d62bc414431b694df9dcd7e93d9
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-16 12:27:22 +0200

    ui.h: Adjust function settings_menu
    Closes https://github.com/Kagamihime/VIlain/issues/109

--- include/ui.h
+++ include/ui.h
@@ -11,7 +11,7 @@ void print_menu(WINDOW * menu_win, int highlight, char **choices,
 int move_in_menu(WINDOW * menu_win, int highlight, char **menu_choices,
                  int number_choices);
 void shortcuts_menu(SETTINGS * sets);
-void settings_menu(SETTINGS * set, BUFFER * buff);
+void settings_menu(SETTINGS * set);
 void print_text(BUFFER * buff, unsigned int first_line, unsigned int first_col);
 void print_wrapped_text(BUFFER * buff, unsigned int first_line);
 void move_cursor(BUFFER * buff, CURSOR * curs, int ch);
