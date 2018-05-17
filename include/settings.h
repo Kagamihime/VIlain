bebab689cc779e07a6c2cf2bb281ff4187c20c97
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-17 12:29:02 +0200

    settings.h: Adjust API for auto_fill_mode
    Closes https://github.com/Kagamihime/VIlain/issues/98

--- include/settings.h
+++ include/settings.h
@@ -10,7 +10,7 @@ char *to_string(int code);
 int is_current_shortcut(SETTINGS * sets, int code);
 int valid_config(SETTINGS * sets, char *path);
 
-int get_line_wrapping(SETTINGS * sets);
+int get_auto_fill_mode(SETTINGS * sets);
 int get_save_shortcut(SETTINGS * sets);
 int get_load_shortcut(SETTINGS * sets);
 int get_settings_shortcut(SETTINGS * sets);
@@ -19,7 +19,7 @@ int get_cut_shortcut(SETTINGS * sets);
 int get_paste_shortcut(SETTINGS * sets);
 int get_toogle_selection_shortcut(SETTINGS * sets);
 
-void set_line_wrapping(SETTINGS * sets, int val);
+void set_auto_fill_mode(SETTINGS * sets, int val);
 void set_save_shortcut(SETTINGS * sets, int val);
 void set_load_shortcut(SETTINGS * sets, int val);
 void set_settings_shortcut(SETTINGS * sets, int val);
