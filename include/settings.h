4601e913b82436338715bfe72240c677fa9ff9ff
Gaëlle Marais <marais.gaelle@gmail.com>
2018-05-18 18:52:57 +0200

    settings.h: Change name is_valid_config
    Closes https://github.com/Kagamihime/VIlain/issues/63

--- include/settings.h
+++ include/settings.h
@@ -8,7 +8,7 @@ int load(SETTINGS * sets, char *path);
 SETTINGS *new_sets();
 char *to_string(int code);
 int is_current_shortcut(SETTINGS * sets, int code);
-int valid_config(SETTINGS * sets, char *path);
+int is_valid_config(SETTINGS * sets, char *path);
 
 int get_auto_fill_mode(SETTINGS * sets);
 int get_save_shortcut(SETTINGS * sets);
